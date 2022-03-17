9f6ea0278c2ba68a2092144b55e03d3dead557e9
VforValens <8889280+VforValens@users.noreply.github.com>
2022-03-17 00:46:10 -0500

    Add files via upload

---
+++ Patches.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using Aki.Reflection.Patching;
+using Aki.Reflection.Utils;
+using EFT;
+
+namespace ValensHasThePower
+{
+    public class PatchThePower : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            var result = typeof(MainApplication).GetMethod("method_41", PatchConstants.PrivateFlags);
+
+            return result;
+        }
+
+        [PatchPostfix]
+        private static void PatchPostFix()
+        {
+            PowerOn.Start();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using BepInEx;
+using UnityEngine;
+using EFT.Interactive;
+using System.Linq;
+
+namespace ValensHasThePower
+{
+    [BepInPlugin("com.Valens.HasThePower", "ValensHasThePower", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            Logger.LogInfo($"Valens...now has the POWER!!!");
+            new PatchThePower().Enable();
+        }
+    }
+
+    public class PowerOn
+    {
+        public static void Start()
+        {
+            var power = GameObject.FindObjectsOfType<Switch>().ToList();
+
+            foreach (var Switch in power) // Turn the Power Station switch to On.
+            {
+                //Interchange - Power to Mall
+                if (Switch.Id == "Shopping_Mall_DesignStuff_00055"&&Switch.name == "reserve_electric_switcher_lever")
+                {
+                    Switch.DoorState = EDoorState.Open;
+                    Debug.LogError(Switch.name);
+                }
+                //Activation of Toilet Key Card Reader in Burger Stop
+                if (Switch.name == "Node_Keycard_Saferoom")
+                {
+                    Switch.DoorState = EDoorState.Open;
+                }
+                //Activation of OBJ21 Container
+                if (Switch.Id == "Shopping_Mall_DesignStuff_00061"&&Switch.Id == "reserve_electric_switcher_lever")
+                {
+                    Switch.DoorState = EDoorState.Open;
+                }
+
+                //Customs - Power to ZB-013 Extract.
+                if (Switch.Id == "custom_DesignStuff_00034"&&Switch.name == "reserve_electric_switcher_lever")
+                {
+                    Switch.DoorState = EDoorState.Open;
+                }
+
+                if (Switch.ExfiltrationPoint == "EXFIL_ZONE_ZB013")
+                {
+                    Switch.ExfiltrationPoint.Status = ??
+                }    
+
+                //Reserve
+            }
+        }
+    }
+}
